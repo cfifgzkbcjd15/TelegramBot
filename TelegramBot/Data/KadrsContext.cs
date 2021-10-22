b7474a3ed01f182b6636138b550a81d94221c20f
Александр Золоторёв <cfifgzkbcjd@mail.ru>
2021-10-22 11:05:48 +0300

    Добавьте файлы проекта.

---
+++ TelegramBot/Data/KadrsContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace TelegramBot.Data
+{
+    public partial class KadrsContext : DbContext
+    {
+        public KadrsContext()
+        {
+        }
+
+        public KadrsContext(DbContextOptions<KadrsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Raspisanie> Raspisanies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-4312I2K;Database=Kadrs;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Cyrillic_General_CI_AS");
+
+            modelBuilder.Entity<Raspisanie>(entity =>
+            {
+                entity.ToTable("Raspisanie");
+
+                entity.Property(e => e.OneGruppa).HasColumnName("One_gruppa");
+
+                entity.Property(e => e.TwoGruppa).HasColumnName("Two_gruppa");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ TelegramBot/Models/Raspisanie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Models
+{
+    class Raspisanie
+    {
+        public int Id { get; set; }
+        public string One_gruppa { get; set; }
+        public string Two_gruppa { get; set; }
+        public int Course { get; set; }
+        public string Profil { get; set; }
+        public string Faculty { get; set; }
+        public string Level { get; set; }
+        public string Day { get; set; }
+        public string Year { get; set; }
+        public int Para { get; set; }
+        public string Time { get; set; }
+        
+
+    }
+}
